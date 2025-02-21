5e78eab733ce5be34c86f91f8bd6e25e64efab0e
Rinvernal <irina.elkina2006@gmail.com>
2025-02-21 11:31:08 +0200

    fixed bug1

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,16 +2,22 @@ import { createContact, deleteContact, getAllContacts, getContactById, updateCon
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 import { parseSortParams } from "../utils/parseSortParams.js";
+import { parseFilterParams } from "../utils/parseFilterParams.js";
 
 export const getContactsController = async (req, res) => {
+  const { _id: userId } = req.user;
   const {page, perPage} = parsePaginationParams(req.query);
   const {sortBy, sortOrder} = parseSortParams(req.query)
-    const contacts = await getAllContacts({
-      page,
-      perPage,
-      sortBy,
-      sortOrder
-    })
+  const filter = parseFilterParams(req.query);
+
+  const contacts = await getAllContacts({
+    userId,
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter
+  })
 
     res.json({
       status: 200,
@@ -21,67 +27,59 @@ export const getContactsController = async (req, res) => {
 }
 
 export const getContactByIdController = async (req, res) => {
-  const {contactId} = req.params;
-  const contact = await getContactById(contactId)
+  const { _id: userId } = req.user;
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId, userId);
 
-  if(!contact) {
-    throw createHttpError(404, 'Contact not found')
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.json({
     status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact
-  })
-}
+    message: `Successfully found contact with id ${contactId}`,
+    data: contact,
+  });
+};
 
 export const createContactController = async (req, res) => {
-  const contact = await createContact(req.body)
+  const { _id: userId } = req.user;
+  const contact = await createContact(userId, req.body);
 
   res.status(201).json({
     status: 201,
     message: `Successfully created a contact!`,
-    data: contact
-  })
-}
+    data: contact,
+  });
+};
 
-export const deleteContactController = async (req, res) => {
-  const {contactId} = req.params
-  const contact = await deleteContact(contactId)
+export const deleteContactController = async (req, res, next) => {
+  const { _id: userId } = req.user;
+  const { contactId } = req.params;
+
+  const contact = await deleteContact(userId, contactId);
 
   if (!contact) {
-    throw createHttpError(404, 'Contact not found')
-  }
-  res.status(204).send()
-}
-
-export const upsertContactController = async (req, res) => {
-  const {contactId} = req.params;
-  const result  = await updateContact(contactId, req.body, {upsert: true})
-
-  if(!result) {
-    throw createHttpError(404, 'Contact not found')
+    next(createHttpError(404, 'Contact not found'));
+    return;
   }
 
-  const status = result.isNew ? 201:200;
-  res.status(status).json({
-    status,
-    message: 'Successfully upserted a contact!',
-    data: result.contact
-  })
-}
+  res.status(204).send();
+};
 
-export const patchContactController = async (req, res) => {
-  const {contactId} = req.params
-  const result = await updateContact(contactId, req.body)
+export const patchContactController = async (req, res, next) => {
+  const { _id: userId } = req.user;
+  const { contactId } = req.params;
+  const result = await updateContact(userId, contactId, req.body);
 
-  if(!result) {
-    throw createHttpError(404, 'Contact not found')
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
   }
 
   res.json({
     status: 200,
-    message: 'Successfully patched a contact!',
-    data: result.contact
-  })
-}
+    message: `Successfully patched a contact!`,
+    data: result.contact,
+  });
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,29 +1,27 @@
 import { Router } from "express";
-import { createContactController, deleteContactController, getContactByIdController, getContactsController, patchContactController, upsertContactController } from "../controllers/contacts.js";
+import { createContactController, deleteContactController, getContactByIdController, getContactsController, patchContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
-import { checkRoles } from "../middlewares/checkRoles.js";
-import { ROLES } from "../constants/index.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
 
-router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getContactsController));
-router.get('/:contactId', checkRoles(ROLES.TEACHER, ROLES.PARENT), isValidId, ctrlWrapper(getContactByIdController))
-router.delete('/:contactId', checkRoles(ROLES.TEACHER), isValidId, ctrlWrapper(deleteContactController))
-router.patch('/:contactId', checkRoles(ROLES.TEACHER, ROLES.PARENT), isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController))
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 router.post(
   '/',
-  checkRoles(ROLES.TEACHER),
   validateBody(createContactSchema),
-  ctrlWrapper(createContactController)
-)
-router.put(
-  '/:contactId',
-  checkRoles(ROLES.TEACHER),
-  isValidId,
-  validateBody(createContactSchema),
-  ctrlWrapper(upsertContactController)
-)
+  ctrlWrapper(createContactController),
+);
 export default router;
